878e6e7b31ed39fe29192fc557fe37391d1c87e2
Kleber Moreti <kleber.camargo@fatec.sp.gov.br>
2018-06-07 22:25:45 -0300

    Added the use of google maps api and did some visual improvements

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
-    const temps = cityData.list.map(weather => weather.main.temp);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
     const pressures = cityData.list.map(weather => weather.main.pressure);
-    const humidities = cityData.list.map(weather => weather.main.humidity);
-
+    const humidities = cityData.list.map(weather => weather.main.humidity);    
+    const { lon, lat } = cityData.city.coord;
+    
     const name = cityData.city.name;
     return (
       <tr key={name}>
-        <td>{name}</td>
-        <td><Chart data={temps} color="orange" /></td>
-        <td><Chart data={pressures} color="green" /></td>          
-        <td><Chart data={humidities} color="blue" /></td> 
+        <td><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="orange" units="°C"/></td>
+        <td><Chart data={pressures} color="green" units="hPa"/></td>          
+        <td><Chart data={humidities} color="blue" units="%"/></td> 
       </tr>
     );
   }
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,7 +5,8 @@ export default function(state = [], action) {
   switch(action.type) {
     case FETCH_WEATHER:
     //console.log(action.payload);
-      return state.concat([action.payload.data]);
+    //return state.concat([action.payload.data]);
+      return [action.payload.data, ...state];
   }
   return state;
 }
